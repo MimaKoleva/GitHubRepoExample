591402ae6856359f593d439abc17b86f5dbad8ed
Mima Koleva <koleva_m@mail.bg>
2022-02-26 13:23:52 +0200

    Add project files.

---
+++ GitHubRepoExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHubRepoExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
